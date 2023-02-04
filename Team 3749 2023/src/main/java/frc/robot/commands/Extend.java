a88021d125ef898d93a2fa73b4c12e56f896b946
Ryan R McWeeny <Ryanrob327@gmail.com>
2023-02-04 15:17:23 -0800

    added comments and authors to commands

--- Team 3749 2023/src/main/java/frc/robot/commands/Extend.java
+++ Team 3749 2023/src/main/java/frc/robot/commands/Extend.java
@@ -4,24 +4,36 @@ import frc.robot.utils.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Extend extends CommandBase {
-    private final Elevator m_elevator;
-    
+/***
+ * @author Ryan McWeeny
+ * @author Anusha Khobare
+ * 
+ *         Command uses PID controller to spin motor that extends elevator
+ */
 
+public class Extend extends CommandBase {
+    // declares Elevator subsystem
+    private final Elevator m_elevator;
+
+    // Initializes the Extend command
     public Extend(Elevator elevator) {
         m_elevator = elevator;
         addRequirements(elevator);
     }
 
+    // Run on command init
     @Override
     public void initialize() {
     }
 
+    // Run every 20 ms
     @Override
     public void execute() {
+        // refences PID controller in Elevator subsystem
         m_elevator.setSpeed(Constants.setpoint_velocity);
     }
 
+    // Run on command finish
     @Override
     public void end(boolean interrupted) {
         m_elevator.setSpeed(Constants.stop_velocity); //set speed to 0 to stop
