d3118f93d506385bf68f605d314bd1133e314229
Ryan R McWeeny <Ryanrob327@gmail.com>
2023-02-04 16:03:52 -0800

    rotate arm commands

---
+++ Team 3749 2023/src/main/java/frc/robot/commands/RotateUp.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import frc.robot.utils.Constants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/***
+ * @author Ryan McWeeny
+ * 
+ *         Rotates a motor that controls the arm
+ */
+
+public class RotateUp {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+
+    Base base = new Base();
+
+    // Initializes the BaseCommand
+    public RotateUp(Arm arm) {
+        this.arm = arm;
+        addRequirements(arm);
+    }
+
+    // Run on command init
+    @Override
+    public void initialize() {
+    }
+
+    // Run every 20 ms
+    @Override
+    public void execute() {
+        base.set(Constants.arm.speed.get().doubleValue());
+    }
+
+    // Run on command finish
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
