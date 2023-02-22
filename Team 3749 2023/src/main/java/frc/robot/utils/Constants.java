0eb82c8a9aa0ae0ff1de4e8f10856c08e41fddbe
advikg <advik.sd@gmail.com>
2023-02-22 15:11:12 -0800

    Updated deps + changed pixels to 240

--- Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
+++ Team 3749 2023/src/main/java/frc/robot/utils/Constants.java
@@ -22,6 +22,6 @@ public final class Constants {
     public static class VisionConstants {
         public static final int apriltag_pipeline_index = 0;
         public static final int reflective_tape_pipeline_index = 1;
-        public static final double limelight_pxls = 1920;
+        public static final double limelight_pxls = 240;
       }
 }
